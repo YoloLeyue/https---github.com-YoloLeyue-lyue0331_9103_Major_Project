832b3aeca06f31f4ee8cb24f3102c2c89b5a0902
LeYue <lyue0331@uni.sydney.edu.au>
2023-11-06 23:57:29 +1100

    set up

--- sketch.js
+++ sketch.js
@@ -1,101 +1,73 @@
-// let url = 'https://coolors.co/072ac8-1e96fc-a2d6f9-fcf300-ffc600';
-// let palette = url.replace('https://coolors.co/', '').split('-').map(c => '#' + c);
-let num = 32;
-let size;
-let step;
-let colorArr = [];
-let sizeArr = [];
-let vmin;
-let img;
-let pg;
+let num = 32; // 方块数量，确保是一个平方数以创建一个平方阵列
+let size; // 每个方块的大小
+let step; // 方块之间的步进值
+let vmin; // 画布的最小维度
+let theScreamImage; // 用于加载图片的变量
+let sampleSound; // 用于加载声音的变量
+let pg; // 图形缓冲区
+
+let button; // 播放按钮
+let fft; // FFT分析对象
+let song; // 歌曲对象
 
 function preload() {
-  img = loadImage('lion.png');
-  // img = loadImage('assets/Edvard_Munch_The_Scream.jpg');
+  theScreamImage = loadImage('assets/Edvard_Munch_The_Scream.jpg');
+  sampleSound = loadSound('assets/sample.mp3');
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
-	pixelDensity(1);
-  ortho(-width / 2, width / 2, height / 2, -height / 2, 0, 2000);
-  
-  vmin = (width < height) ? width : height;
-  size = vmin / (num * 4);
-  step = size * 4;
-  img.resize(num, num);
+  fft = new p5.FFT(0.8, 256); // 使用256个频段
+  song = sampleSound;
+  song.connect(fft);
+
+  button = createButton('play');
+  button.mousePressed(startAudio);
+
+  vmin = min(width, height);
+  size = vmin / num;
+  step = size;
+
+  theScreamImage.resize(num, num);
   pg = createGraphics(num, num);
-  pg.image(img, 0, 0, pg.width, pg.height);
-  pg.loadPixels();
-  
-  for (let i = 0; i < num * num; i++) {
-    let r = pg.pixels[i * 4 + 0];
-    let g = pg.pixels[i * 4 + 1];
-    let b = pg.pixels[i * 4 + 2];
-    let a = pg.pixels[i * 4 + 3];
-    colorArr.push(color(r, g, b));
-    sizeArr.push(map(r+g+b, 0, 255*2, 0, 4, true) * (a / 255));
+  pg.image(theScreamImage, 0, 0, num, num);
+}
+
+function startAudio() {
+  if (getAudioContext().state !== 'running') {
+    userStartAudio();
+  }
+  if (song.isPlaying()) {
+    song.pause();
+    button.html('play');
+  } else {
+    song.loop();
+    button.html('pause');
   }
 }
 
 function draw() {
   background(31, 19, 1);
-  let fc = frameCount * 0.02;
-  let sn = sin(fc) * 0.5 + 0.5;
-  rotateX(PI * (0.5 - sn * 0.35));
-  rotateY(PI * (sn * 0.25));
-  
-  ambientLight(160, 160, 160);
-  directionalLight(200, 200, 200, 1, -1, -1);
-  
-  noStroke();
-  
+  let spectrum = fft.analyze(); // 获取频谱数据
+
+  // 根据频谱数据动态调整方块高度
   for (let i = 0; i < num; i++) {
-    let z = i * step;
-    let pz = (i - num / 2 + 0.5) * step;
     for (let j = 0; j < num; j++) {
-      let x = j * step;
-      let px = (j - num / 2 + 0.5) * step;
-      let idx = i * num + j;
-      let sz = size * sizeArr[idx];
-      let s = floor(sizeArr[idx] * 2 * sn * 2) / 2;
-      if (s < 0.5) continue;
-      fill(colorArr[idx]);
+      // 根据距离中心的远近选择频谱中的数据
+      let d = dist(i, j, num / 2, num / 2);
+      let index = floor(map(d, 0, dist(0, 0, num / 2, num / 2), 0, spectrum.length - 1));
+      let amp = spectrum[index]; // 频谱中的振幅值
+      let sz = map(amp, 0, 256, size / 2, size * 2); // 映射振幅到方块的高度
+
+      let x = map(i, 0, num - 1, -width / 2, width / 2);
+      let y = map(j, 0, num - 1, -height / 2, height / 2);
+
+      let c = pg.get(i, j); // 获取对应像素的颜色
+
       push();
-      {
-        translate(px, sz * s / 2, pz);
-        
-        push();
-        {
-          translate(-sz / 4, sz * s / 2, -sz / 4);
-          scale(sz, sz, sz);
-          cylinder(0.15, 0.2, 6);
-        }
-        pop();
-        push();
-        {
-          translate(sz / 4, sz * s / 2, -sz / 4);
-          scale(sz, sz, sz);
-          cylinder(0.15, 0.2, 6);
-        }
-        pop();
-        push();
-        {
-          translate(sz / 4, sz * s / 2, sz / 4);
-          scale(sz, sz, sz);
-          cylinder(0.15, 0.2, 6);
-        }
-        pop();
-        push();
-        {
-          translate(-sz / 4, sz * s / 2, sz / 4);
-          scale(sz, sz, sz);
-          cylinder(0.15, 0.2, 6);
-        }
-        pop();
-        
-        scale(sz, sz * s, sz);
-        box(1);
-      }
+      translate(x, y, -sz / 2);
+      fill(c);
+      box(size, size, sz);
       pop();
     }
   }
